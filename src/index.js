6c86a220ec6aef627c470d95eb982bd191763547
MarahDeven <91274269+MarahDeven@users.noreply.github.com>
2022-02-15 10:22:21 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,38 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./styles.css";
+
+function App() {
+  let countries = ["France", "USA", "Australia", "Japan"];
+  return (
+    <div className="App">
+      <h1>React Loops</h1>
+
+      <h3>Challenge 1</h3>
+      <p>
+        Create a new Component called BalticCountries and list the 3 baltic
+        countries (Estonia, Latvia, Lithuania) without using loops or variables.
+        Include the component below.
+      </p>
+
+      <h3>Challenge 2</h3>
+      <p>
+        Include a component called Countries below and send the variable
+        countries (created above) as a property. Create a new component called
+        Countries and list all countries from the Countries component using a
+        loop.
+      </p>
+
+      <h3>Challenge 3</h3>
+      <p>
+        Modify the countries variable in the code above so it includes the
+        country name but also the capital city of each country. Display each
+        country and capital city in the Countries component
+      </p>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
